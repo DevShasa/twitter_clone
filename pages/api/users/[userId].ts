97b8beb1fdb43e70e64596d3462276943eba5ff5
DevShasa <shasa.thuo@gmail.com>
2023-04-13 04:21:45 +0300

    user apis

---
+++ pages/api/users/[userId].ts
@@ -0,0 +1,39 @@
+// Get a specific user
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/libs/prismadb"
+
+export default async function handler(req:NextApiRequest, res:NextApiResponse){
+    if(req.method !== "GET"){
+        return res.status(405).end();
+    }
+
+    try {
+        const { userId } = req.query;
+        if(!userId || typeof userId !== 'string'){
+            // there is no userid and its type is not equal to string
+            throw new Error("Invalid ID")
+        }
+
+        const existingUser = await prisma.user.findUnique({
+            where:{
+                id:userId
+            }
+        })
+
+        const followerCount = await prisma.user.count({
+            // Go through all the followingIds and count where userId is in there...
+            // Meaning those people follow this user
+            where:{
+                followingIds:{
+                    has:userId
+                }
+            }
+        })
+
+        return res.status(200).json({...existingUser, followerCount})
+
+    } catch (error) {
+        console.log("Error fetching user with id, from api/users/[userId]")
+        return res.status(400).end()
+    }
+}
