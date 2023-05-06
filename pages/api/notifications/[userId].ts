f71fe6e64ddac117055e0f9111512d4e14ca2703
Shasa <shasa.thuo@gmail.com>
2023-05-06 22:51:00 +0300

    notifications api

--- pages/api/notifications/[userId].ts
+++ pages/api/notifications/[userId].ts
@@ -1,1 +1,33 @@
-import {} from "next"
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/libs/prismadb";
+import { withMethods } from "@/middleware/withMethod";
+
+
+async function handler(req: NextApiRequest, res:NextApiResponse){
+    try {
+        const { userId } = req.query
+        if(!userId || typeof userId !== "string"){throw new Error("Id is invalid api/notification")}
+
+        const notification = await prisma.notification.findMany({
+            where:{userId},
+            orderBy:{
+                createdAt:"desc"
+            }
+        })
+        // after fetching the notification then remove notification flag
+        await prisma.user.update({
+            where:{
+                id:userId
+            },
+            data:{
+                hasNotification: false
+            }
+        })
+        return res.status(200).json(notification)
+    } catch (error) {
+        console.log("Error fetching notifications---->", error)
+        return res.status(400).end();
+    }
+}
+
+export default withMethods(["GET"], handler)
